e8089ae2e5b5acf228b156ce03ecf339fa59129d
camilarolandi <rolandi.camil@gmail.com>
2024-08-13 18:15:22 -0300

    clase

---
+++ Clase 2/src/App.jsx
@@ -0,0 +1,157 @@
+/* cuando escribo JSX hago el import con import */
+import React from 'react'
+/* import {PI,doSomething,Titulos} from "./funciones"
+import {Carrito} from './Components/Carrito/carrito'
+import {Counter} from './Practica/counter'
+import {Text} from './Practica/mostrar-ocultar' */
+
+
+
+
+/* puedo escribir en flecha. pero SIEMPRE mayuscula en la primer letra. ESCRIBO HTML desde APP NO desde index  */
+
+/* function App() { */
+  
+
+/* console.log(PI)
+doSomething() */
+
+
+const personas = ["pepe","juan","maria"]
+const listaPersonas = personas.map((persona)  => {
+  return(
+    <div>{persona}</div>
+  )
+})
+
+
+
+
+
+const App = () => {
+
+return(
+  <div>
+{listaArticulos}
+{listaPersonas}
+
+  </div>
+
+)
+
+
+}
+
+  
+  /* const persona_1 =  {
+      nombre: "pepe",
+      edad: 89,
+      ubicacion: "calle 123",
+
+    }
+  
+    const persona_2 = {
+      nombre: "juan",
+      edad: 20,
+      ubicacion: "calle 456",
+
+    }
+
+  return (
+    <div>
+      /<h1>Hola desde react</h1> 
+      <ProductCard/>
+      <Carrito/>
+      <Titulos/>
+      <Counter/>
+      <Text/> 
+
+      
+
+        <ProfileCard 
+          persona= {persona_1}
+        />
+
+        <ProfileCard 
+          persona= {persona_2}
+        />
+    </div>
+  )
+}
+ */
+/* no se pueden poner varios divs. solo un elemento padre NO puedo hacer esto:
+
+<div> 
+  <h3> Titulo Producto: </h3>
+</div>
+
+<div> 
+  <h3> Titulo Producto: </h3>
+</div>
+
+DEBERIA HACER:
+
+<div> 
+  <div> 
+    <h3> Titulo Producto: </h3>
+  </div>
+</div>
+
+O
+
+USAR UN FRAGMENTO (Para evitar crear elementos padres, se comporta como tal pero no existe en la pagina)
+<>
+
+</>
+
+*/
+
+
+/* lo llamo donde quiero usarlo con </> */
+/* const ProductCard = () => {
+  return (
+
+  <>
+    <div>
+      <h3> Titulo Producto: </h3>
+      <p>Descripcion producto: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa velit quod reiciendis esse voluptatum recusandae nihil officiis quisquam, ullam sequi amet quasi soluta provident impedit praesentium voluptatem, molestiae illum harum. </p>
+      <span> Precio producto: $value</span>
+      <button>Comprar</button>
+    </div>
+    
+    <div> 
+      <button>Like</button>
+      <button>Dislike</button>
+    </div>
+    
+  </>
+
+  )
+
+}
+ */
+
+export default App
+
+
+/* TAREA:
+
+Crear un comoponente Carrito
+h1: carrito de compras
+
+otro componente Item
+
+<div>
+    <h2>nombre producto</h2>
+    <span>Cantidad: x </span>
+    <button>Eliminar</button>
+</div>
+
+mostrarlo en el componente carrito en app
+
+
+*/
+
+
+
+/* clases= className */
